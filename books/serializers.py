8211910488b0cbb1abddf00d7897887990806cc4
struhanets <posseydon87@gmail.com>
2024-12-28 10:52:24 +0100

    Created BooksSerializer

---
+++ books/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from books.models import Books
+
+
+class BooksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Books
+        fields = (
+            "title",
+            "author",
+            "cover",
+            "inventory",
+            "daily_fee",
+        )
