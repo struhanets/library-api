2e4c55f190d2aab98efda8822753e42f72316100
struhanets <posseydon87@gmail.com>
2024-12-28 12:52:44 +0100

    Implemented API router & URL for Borrowings model

---
+++ borrowings/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from borrowings import views
+
+router = routers.DefaultRouter()
+router.register(r"borrowings", views.BorrowingViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "borrowings"
--- city_library_project/urls.py
+++ city_library_project/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("books.urls"), namespace="books"),
+    path("api/borrowings", include("borrowings.urls"), namespace="borrowings")
 ]
