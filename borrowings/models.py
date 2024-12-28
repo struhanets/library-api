48ea18d1cde82da3aa31238164fbaec4df2f7043
struhanets <posseydon87@gmail.com>
2024-12-28 12:50:59 +0100

    Created Borrowings model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models import CheckConstraint, Q, F
 
-# Create your models here.
+from books.models import Books
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField(null=True, blank=True)
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                condition=Q(expected_return_date__gt=F("borrow_date"))
+                | Q(actual_return_date__gt=F("borrow_date")),
+                name="expected_return_date",
+            ),
+            CheckConstraint(
+                condition=Q(actual_return_date__lte=F("expected_return_date")),
+                name="correct_return_date",
+            ),
+        ]
