c76ce05785827e66c7931a56ce4d1adf0d96d1b2
struhanets <posseydon87@gmail.com>
2024-12-29 16:39:28 +0100

    Added permission and serializer for create action

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import permissions
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer
+from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [permissions.IsAuthenticatedOrReadOnly]
+        return [permissions.IsAdminUser]
