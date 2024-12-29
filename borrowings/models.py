408aba2efce8e5d49ec31efbd5052416db37b9db
struhanets <posseydon87@gmail.com>
2024-12-29 16:27:44 +0100

    Changed constraints for Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -9,16 +9,13 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField(null=True, blank=True)
     actual_return_date = models.DateField()
-    book = models.ForeignKey(Books, on_delete=models.CASCADE)
-    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    book = models.ForeignKey(Books, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="customer"
+    )
 
     class Meta:
         constraints = [
-            CheckConstraint(
-                condition=Q(expected_return_date__gt=F("borrow_date"))
-                | Q(actual_return_date__gt=F("borrow_date")),
-                name="expected_return_date",
-            ),
             CheckConstraint(
                 condition=Q(actual_return_date__lte=F("expected_return_date")),
                 name="correct_return_date",
