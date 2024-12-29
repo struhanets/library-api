a4c648c0bba87c8e0adf872d82c66ebf6cdf2af9
struhanets <posseydon87@gmail.com>
2024-12-29 16:37:49 +0100

    Added inventory validation and reduction logic for book borrowing

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
 
+from books.models import Books
 from borrowings.models import Borrowing
-from books.serializers import BooksSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    book = BooksSerializer(read_only=True)
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "user",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+        )
+
+
+class BorrowingCreateSerializer(BorrowingSerializer):
+    book = serializers.SlugRelatedField(
+        slug_field="title", queryset=Books.objects.all()
+    )
 
     class Meta:
         model = Borrowing
@@ -17,7 +32,18 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "actual_return_date",
             "book",
         )
-        read_only_fields = (
-            "id",
-            "user",
-        )
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+
+        # Перевіряємо наявність книг в інвентарі
+        if book.inventory <= 0:
+            raise serializers.ValidationError("No books available in the inventory.")
+
+        # Зменшуємо інвентар на 1
+        book.inventory -= 1
+        book.save()  # Зберігаємо зміни в інвентарі
+
+        borrowing = Borrowing.objects.create(**validated_data)
+
+        return borrowing
