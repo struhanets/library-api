85ce97aa53220bda8c71532001dc1e3f1c44edfc
struhanets <posseydon87@gmail.com>
2024-12-29 10:46:30 +0100

    Added ManageUserView

--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
 
 from user.serializers import UserSerializer
@@ -5,3 +6,12 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
+
+    def get_object(self):
+        return self.request.user
