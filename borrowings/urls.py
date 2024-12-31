f291098cd73da66f3401019ba1d4468bbadf6835
struhanets <posseydon87@gmail.com>
2024-12-31 11:54:38 +0100

    Updated URL for borrowings return option

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 
 from borrowings import views
 from borrowings.views import BorrowingViewSet, BorrowingsRetrieveView
+
 borrowings_list = BorrowingViewSet.as_view(actions={'get': 'list', 'post': 'create'})
-# borrowings_retrieve = BorrowingsRetrieveView.as_view(actions={'get': 'retrieve'})
 
 urlpatterns = [
     path("borrowings/", borrowings_list, name="borrowings-list"),
     path("borrowings/<int:pk>/", BorrowingsRetrieveView.as_view(), name="borrowings-detail"),
-    # path("borrowings/return/", BorrowingViewSet.as_view(), name="borrowings-return")
+    path("borrowings/<int:pk>/return/", BorrowingsRetrieveView.as_view(), name="borrowings-return")
 ]
 
 app_name = "borrowings"
