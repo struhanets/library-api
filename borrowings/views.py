756a81837ccb1f60200516b40f56c1453a6fb126
struhanets <posseydon87@gmail.com>
2025-01-02 13:14:40 +0100

    Implement bot for borrowing notifications

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
 from rest_framework import viewsets, generics
-from rest_framework import permissions
-from rest_framework.generics import RetrieveAPIView
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerializer, BorrowingRetrieveSerializer, \
-    BorrowingReturnSerializer
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingCreateSerializer,
+    BorrowingRetrieveSerializer,
+)
+
+from borrowings.teleg_bot import send_telegram_message
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
@@ -20,8 +22,8 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = self.queryset
 
-        user_id = self.request.query_params.get('user')
-        user_status = self.request.query_params.get('user__is_active')
+        user_id = self.request.query_params.get("user")
+        user_status = self.request.query_params.get("user__is_active")
 
         if user_id:
             user_id = [int(str_id) for str_id in user_id.split(",")]
@@ -34,17 +36,24 @@ class BorrowingViewSet(viewsets.ModelViewSet):
                 user_status = False
 
             else:
-                raise ValueError("Invalid value for user__is_active. Use 'true' or 'false'.")
+                raise ValueError(
+                    "Invalid value for user__is_active. Use 'true' or 'false'."
+                )
 
             queryset = queryset.filter(user__is_active=user_status)
 
         return queryset
 
+    def perform_create(self, serializer):
+        borrowing = serializer.save()
 
-    # def get_permissions(self):
-    #     if self.action == "create":
-    #         return [permissions.IsAuthenticatedOrReadOnly]
-    #     return [permissions.AllowAny]
+        message = (
+            "New borrowing create ",
+            f"user: {borrowing.user.first_name} {borrowing.user.last_name} ",
+            f"By books: {borrowing.book.title}" f"In date: {borrowing.borrow_date}",
+        )
+
+        send_telegram_message(message)
 
 
 class BorrowingsRetrieveView(generics.RetrieveUpdateAPIView):
@@ -57,6 +66,3 @@ class BorrowingsRetrieveView(generics.RetrieveUpdateAPIView):
         if borrowing.actual_return_date:
             borrowing.book.inventory += 1
             borrowing.book.save()
-
-
-
