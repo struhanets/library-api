830c233661196b91b41e5353e2262f14f0214185
struhanets <posseydon87@gmail.com>
2025-01-02 11:09:56 +0100

    Set permissions for Books CRUD actions

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 from books.models import Books
 from books.serializers import BooksSerializer
@@ -8,3 +9,9 @@ from books.serializers import BooksSerializer
 class BooksViewSet(viewsets.ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BooksSerializer
+    permission_classes = (IsAdminUser,)
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return (AllowAny(),)
+        return (IsAdminUser(),)
