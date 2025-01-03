3a64b99106e0f16e0d0fd22d8df99bf6d14fbcc2
struhanets <posseydon87@gmail.com>
2025-01-03 11:14:07 +0100

    Improved Books views documentation

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser, AllowAny
 
@@ -6,12 +6,17 @@ from books.models import Books
 from books.serializers import BooksSerializer
 
 
+@extend_schema(
+    tags=["Books"],
+    description="Endpoints to manage books in the library"
+    "You can create, list, retrieve, update, or delete books",
+)
 class BooksViewSet(viewsets.ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BooksSerializer
     permission_classes = (IsAdminUser,)
 
     def get_permissions(self):
-        if self.action == 'list':
+        if self.action == "list":
             return (AllowAny(),)
         return (IsAdminUser(),)
